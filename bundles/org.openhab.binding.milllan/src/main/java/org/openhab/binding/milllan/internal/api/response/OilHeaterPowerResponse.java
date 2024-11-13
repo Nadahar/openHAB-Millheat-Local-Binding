bb6574abc4630293560e2c1157078112d14b075a
Nadahar <Nadahar@users.noreply.github.com>
2024-11-13 20:54:45 +0100

    more

---
+++ bundles/org.openhab.binding.milllan/src/main/java/org/openhab/binding/milllan/internal/api/response/OilHeaterPowerResponse.java
@@ -0,0 +1,71 @@
+package org.openhab.binding.milllan.internal.api.response;
+
+import java.util.Objects;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.Nullable;
+import org.openhab.binding.milllan.internal.api.ResponseStatus;
+
+
+/**
+ * This class is used for deserializing JSON response objects from the "oil-heater-power" API call.
+ *
+ * @author Nadahar
+ */
+@NonNullByDefault
+public class OilHeaterPowerResponse implements Response { // TODO: (Nad) Header + JavaDocs
+
+    /**
+     * The limited heating power percentage value.
+     */
+    @Nullable
+    private Integer value;
+
+    @Nullable
+    private ResponseStatus status;
+
+    @Nullable
+    public Integer getValue() {
+        return value;
+    }
+
+    @Override
+    @Nullable
+    public ResponseStatus getStatus() {
+        return status;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(status, value);
+    }
+
+    @Override
+    public boolean equals(@Nullable Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        OilHeaterPowerResponse other = (OilHeaterPowerResponse) obj;
+        return status == other.status && Objects.equals(value, other.value);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        builder.append("OilHeaterPowerResponse [");
+        if (value != null) {
+            builder.append("value=").append(value).append(", ");
+        }
+        if (status != null) {
+            builder.append("status=").append(status);
+        }
+        builder.append("]");
+        return builder.toString();
+    }
+}
