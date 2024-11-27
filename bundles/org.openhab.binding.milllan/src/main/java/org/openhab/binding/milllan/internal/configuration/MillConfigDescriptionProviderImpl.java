8a28e1a383b14780d66c66730805c3c023f1dbc3
Nadahar <Nadahar@users.noreply.github.com>
2024-11-27 19:01:02 +0100

    configuration localization wip

--- bundles/org.openhab.binding.milllan/src/main/java/org/openhab/binding/milllan/internal/configuration/MillConfigDescriptionProviderImpl.java
+++ bundles/org.openhab.binding.milllan/src/main/java/org/openhab/binding/milllan/internal/configuration/MillConfigDescriptionProviderImpl.java
@@ -15,6 +15,7 @@ import java.util.Map.Entry;
 
 import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.eclipse.jdt.annotation.Nullable;
+import org.openhab.binding.milllan.internal.MillBindingConstants;
 import org.openhab.binding.milllan.internal.MillUtil;
 import org.openhab.core.config.core.ConfigDescription;
 import org.openhab.core.config.core.ConfigDescriptionBuilder;
@@ -22,17 +23,17 @@ import org.openhab.core.config.core.ConfigDescriptionParameter;
 import org.openhab.core.config.core.ConfigDescriptionParameter.Type;
 import org.openhab.core.config.core.ConfigDescriptionParameterBuilder;
 import org.openhab.core.config.core.ConfigDescriptionProvider;
-import org.openhab.core.config.core.ConfigOptionProvider;
-import org.openhab.core.config.core.ParameterOption;
+import org.openhab.core.i18n.TranslationProvider;
 import org.openhab.core.thing.ThingUID;
+import org.osgi.framework.Bundle;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 @NonNullByDefault
-@Component(service = {ConfigDescriptionProvider.class, ConfigOptionProvider.class, MillConfigDescriptionProvider.class})
-public class MillConfigDescriptionProviderImpl implements ConfigDescriptionProvider, ConfigOptionProvider, MillConfigDescriptionProvider {
+@Component(service = {ConfigDescriptionProvider.class, MillConfigDescriptionProvider.class})
+public class MillConfigDescriptionProviderImpl implements ConfigDescriptionProvider, MillConfigDescriptionProvider {
 
     private final Logger logger = LoggerFactory.getLogger(MillConfigDescriptionProviderImpl.class);
 
@@ -44,18 +45,6 @@ public class MillConfigDescriptionProviderImpl implements ConfigDescriptionProvi
 
     @Activate
     public MillConfigDescriptionProviderImpl() {
-    }
-
-    @Override
-    public @Nullable Collection<ParameterOption> getParameterOptions(
-        URI uri,
-        String param,
-        @Nullable String context,
-        @Nullable Locale locale
-    ) {
-        // TODO: (Nad) Implement or remove interface
-//        logger.debug("getParameterOptions(), uri=\"{}\", param=\"{}\", context={}", uri, param, context);
-        return null;
     }
 
     @Override
@@ -367,5 +356,281 @@ public class MillConfigDescriptionProviderImpl implements ConfigDescriptionProvi
             }
         }
         return "";
+    }
+
+    public static enum DescriptionParameter {
+        CONFIG_PARAM_TIMEZONE_OFFSET(
+            MillBindingConstants.CONFIG_PARAM_TIMEZONE_OFFSET,
+            "thing-general.config.milllan.timezone-offset.label",
+            "Time Zone Offset",
+            "thing-general.config.milllan.timezone-offset.description",
+            "The time zone offset from UTC in minutes."
+        ),
+        CONFIG_PARAM_PID_KP(
+            MillBindingConstants.CONFIG_PARAM_PID_KP,
+            "",
+            "Kp",
+            "thing-general.config.milllan.pid-kp.description",
+            "The PID controller's proportional gain factor K<sub>p</sub>."
+        ),
+        CONFIG_PARAM_PID_KI(
+            MillBindingConstants.CONFIG_PARAM_PID_KI,
+            "",
+            "Ki",
+            "thing-general.config.milllan.pid-ki.description",
+            "The PID controller's integral gain factor K<sub>i</sub>."
+        ),
+        CONFIG_PARAM_PID_KD(
+            MillBindingConstants.CONFIG_PARAM_PID_KD,
+            "",
+            "Kd",
+            "thing-general.config.milllan.pid-kd.description",
+            "The PID controller's derivative gain factor K<sub>d</sub>."
+        ),
+        CONFIG_PARAM_PID_KD_FILTER_N(
+            MillBindingConstants.CONFIG_PARAM_PID_KD_FILTER_N,
+            "thing-general.config.milllan.pid-kd-filter.label",
+            "Kd Filter",
+            "thing-general.config.milllan.pid-kd-filter.description",
+            "The PID controller's derivative (K<sub>d</sub>) filter time coefficient."
+        ),
+        CONFIG_PARAM_PID_WINDUP_LIMIT_PCT(
+            MillBindingConstants.CONFIG_PARAM_PID_WINDUP_LIMIT_PCT,
+            "thing-general.config.milllan.pid-kd-wind-up.label",
+            "Ki Wind-up Limit",
+            "thing-general.config.milllan.pid-kd-wind-up.description",
+            "The PID controller's wind-up limit for integral part (K<sub>i</sub>) in percent (0 to 100)."
+        ),
+        CONFIG_PARAM_CLOUD_COMMUNICATION(
+            MillBindingConstants.CONFIG_PARAM_CLOUD_COMMUNICATION,
+            "thing-general.config.milllan.cloud-communication.label",
+            "Enable Cloud Communication",
+            "thing-general.config.milllan.cloud-communication.description",
+            "Whether cloud communication is enabled in the device. Changing this will reboot the device."
+        ),
+        CONFIG_PARAM_HYSTERESIS_UPPER(
+            MillBindingConstants.CONFIG_PARAM_HYSTERESIS_UPPER,
+            "thing-general.config.milllan.hysteresis-upper.label",
+            "Hysteresis Upper",
+            "thing-general.config.milllan.hysteresis-upper.description",
+            "The upper limit: Set temperature + upper limit = stop heating."
+        ),
+        CONFIG_PARAM_HYSTERESIS_LOWER(
+            MillBindingConstants.CONFIG_PARAM_HYSTERESIS_LOWER,
+            "thing-general.config.milllan.hysteresis-lower.label",
+            "Hysteresis Lower",
+            "thing-general.config.milllan.hysteresis-lower.description",
+            "The lower limit: Set temperature - lower limit = start heating."
+        ),
+        CONFIG_PARAM_COMMERCIAL_LOCK_MIN(
+            MillBindingConstants.CONFIG_PARAM_COMMERCIAL_LOCK_MIN,
+            "thing-general.config.milllan.commercial-lock-min.label",
+            "Commercial Lock Minimum Temperature",
+            "thing-general.config.milllan.commercial-lock-min.description",
+            "The minimum temperature that can be set while the commercial lock is active."
+        ),
+        CONFIG_PARAM_COMMERCIAL_LOCK_MAX(
+            MillBindingConstants.CONFIG_PARAM_COMMERCIAL_LOCK_MAX,
+            "thing-general.config.milllan.commercial-lock-max.label",
+            "Commercial Lock Maximum Temperature",
+            "thing-general.config.milllan.commercial-lock-max.description",
+            "The maximum temperature that can be set while the commercial lock is active."
+        ),
+        CONFIG_PARAM_OPEN_WINDOW_DROP_TEMP_THR(
+            MillBindingConstants.CONFIG_PARAM_OPEN_WINDOW_DROP_TEMP_THR,
+            "thing-general.config.milllan.open-window-drop-temp-thr.label",
+            "Drop Temperature Threshold",
+            "thing-general.config.milllan.open-window-drop-temp-thr.description",
+            "The temperature drop required to trigger (activate) the open window function."
+        ),
+        CONFIG_PARAM_OPEN_WINDOW_DROP_TIME_RANGE(
+            MillBindingConstants.CONFIG_PARAM_OPEN_WINDOW_DROP_TIME_RANGE,
+            "thing-general.config.milllan.open-window-drop-time-range.label",
+            "Drop Time Range",
+            "thing-general.config.milllan.open-window-drop-time-range.description",
+            "The time range for which a drop in temperature will be evaluated."
+        ),
+        CONFIG_PARAM_OPEN_WINDOW_INC_TEMP_THR(
+            MillBindingConstants.CONFIG_PARAM_OPEN_WINDOW_INC_TEMP_THR,
+            "thing-general.config.milllan.open-window-inc-temp-thr.label",
+            "Increase Temperature Threshold",
+            "thing-general.config.milllan.open-window-inc-temp-thr.description",
+            "The temperature increase required to deactivate the open window function."
+        ),
+        CONFIG_PARAM_OPEN_WINDOW_INC_TIME_RANGE(
+            MillBindingConstants.CONFIG_PARAM_OPEN_WINDOW_INC_TIME_RANGE,
+            "thing-general.config.milllan.open-window-inc-time-range.label",
+            "Increase Time Range",
+            "thing-general.config.milllan.open-window-inc-time-range.description",
+            "The time range for which an increase in temperature will be evaluated."
+        ),
+        CONFIG_PARAM_OPEN_WINDOW_MAX_TIME(
+            MillBindingConstants.CONFIG_PARAM_OPEN_WINDOW_MAX_TIME,
+            "thing-general.config.milllan.open-window-max-time.label",
+            "Maximum Time",
+            "thing-general.config.milllan.open-window-max-time.description",
+            "The maximum time the open window function will remain active."
+        );
+
+        private final String name;
+
+        private final String labelKey;
+        private final String defaultLabel;
+        private final String descrKey;
+        private final String defaultDescr;
+
+        private DescriptionParameter(
+            String name,
+            String labelKey,
+            String defaultLabel,
+            String descrKey,
+            String defaultDescr
+        ) {
+            this.name = name;
+            this.labelKey = labelKey;
+            this.defaultLabel = defaultLabel;
+            this.descrKey = descrKey;
+            this.defaultDescr = defaultDescr;
+        }
+
+        public ConfigDescriptionParameter getConfigDescriptionParameter(
+            @Nullable Bundle bundle,
+            @Nullable TranslationProvider provider,
+            @Nullable Locale locale
+        ) {
+            ConfigDescriptionParameterBuilder builder;
+            switch (this) {
+                case CONFIG_PARAM_TIMEZONE_OFFSET:
+                    builder = ConfigDescriptionParameterBuilder.create(name, Type.INTEGER)
+                        .withUnit("min").withMinimum(BigDecimal.valueOf(-840L))
+                        .withMaximum(BigDecimal.valueOf(720L)).withStepSize(BigDecimal.valueOf(15L))
+                        .withGroupName("general").withAdvanced(true).withDefault("0");
+                    break;
+                case CONFIG_PARAM_PID_KP:
+                    builder = ConfigDescriptionParameterBuilder.create(name, Type.DECIMAL)
+                        .withMinimum(BigDecimal.valueOf(0L)).withStepSize(BigDecimal.valueOf(1L))
+                        .withGroupName("pid").withAdvanced(true).withDefault("70").withLabel(defaultLabel);
+
+                    if (bundle == null || provider == null || locale == null) {
+                        builder.withDescription(defaultDescr);
+                    } else {
+                        builder.withDescription(provider.getText(bundle, descrKey, defaultDescr, locale));
+                    }
+                    return builder.build();
+                case CONFIG_PARAM_PID_KI:
+                    builder = ConfigDescriptionParameterBuilder.create(name, Type.DECIMAL)
+                        .withMinimum(BigDecimal.valueOf(0L)).withStepSize(BigDecimal.valueOf(0.01))
+                        .withGroupName("pid").withAdvanced(true).withDefault("0.02").withLabel(defaultLabel);
+                    if (bundle == null || provider == null || locale == null) {
+                        builder.withDescription(defaultDescr);
+                    } else {
+                        builder.withDescription(provider.getText(bundle, descrKey, defaultDescr, locale));
+                    }
+                    return builder.build();
+                case CONFIG_PARAM_PID_KD:
+                    builder = ConfigDescriptionParameterBuilder.create(name, Type.DECIMAL)
+                        .withMinimum(BigDecimal.valueOf(0L)).withStepSize(BigDecimal.valueOf(1L))
+                        .withGroupName("pid").withAdvanced(true).withDefault("4500").withLabel(defaultLabel);
+                    if (bundle == null || provider == null || locale == null) {
+                        builder.withDescription(defaultDescr);
+                    } else {
+                        builder.withDescription(provider.getText(bundle, descrKey, defaultDescr, locale));
+                    }
+                    return builder.build();
+                case CONFIG_PARAM_PID_KD_FILTER_N:
+                    builder = ConfigDescriptionParameterBuilder.create(name, Type.DECIMAL)
+                        .withMinimum(BigDecimal.valueOf(0L)).withStepSize(BigDecimal.valueOf(1L))
+                        .withGroupName("pid").withAdvanced(true).withDefault("60");
+                    break;
+                case CONFIG_PARAM_PID_WINDUP_LIMIT_PCT:
+                    builder = ConfigDescriptionParameterBuilder.create(name, Type.DECIMAL)
+                        .withMinimum(BigDecimal.valueOf(0L)).withMaximum(BigDecimal.valueOf(100L))
+                        .withStepSize(BigDecimal.valueOf(1L)).withUnit("%")
+                        .withGroupName("pid").withAdvanced(true).withDefault("95");
+                    break;
+                case CONFIG_PARAM_CLOUD_COMMUNICATION:
+                    builder = ConfigDescriptionParameterBuilder.create(name, Type.BOOLEAN)
+                        .withGroupName("general").withAdvanced(true).withDefault("false");
+                    break;
+                case CONFIG_PARAM_HYSTERESIS_UPPER:
+                    builder = ConfigDescriptionParameterBuilder.create(name, Type.DECIMAL)
+                        .withMinimum(BigDecimal.valueOf(0L)).withMaximum(BigDecimal.valueOf(5L))
+                        .withStepSize(BigDecimal.valueOf(0.25)).withGroupName("hysteresis")
+                        .withAdvanced(true).withDefault("1");
+                    break;
+                case CONFIG_PARAM_HYSTERESIS_LOWER:
+                    builder = ConfigDescriptionParameterBuilder.create(name, Type.DECIMAL)
+                        .withMinimum(BigDecimal.valueOf(0L)).withMaximum(BigDecimal.valueOf(5L))
+                        .withStepSize(BigDecimal.valueOf(0.25)).withGroupName("hysteresis")
+                        .withAdvanced(true).withDefault("0.5");
+                    break;
+                case CONFIG_PARAM_COMMERCIAL_LOCK_MIN:
+                    builder = ConfigDescriptionParameterBuilder.create(name, Type.DECIMAL)
+                        .withMinimum(BigDecimal.valueOf(0L)).withMaximum(BigDecimal.valueOf(99L))
+                        .withStepSize(BigDecimal.valueOf(0.5)).withGroupName("commercialLock")
+                        .withAdvanced(true).withDefault("21");
+                    break;
+                case CONFIG_PARAM_COMMERCIAL_LOCK_MAX:
+                    builder = ConfigDescriptionParameterBuilder.create(name, Type.DECIMAL)
+                        .withMinimum(BigDecimal.valueOf(0L)).withMaximum(BigDecimal.valueOf(99L))
+                        .withStepSize(BigDecimal.valueOf(0.5)).withGroupName("commercialLock")
+                        .withAdvanced(true).withDefault("23");
+                    break;
+                case CONFIG_PARAM_OPEN_WINDOW_DROP_TEMP_THR:
+                    builder = ConfigDescriptionParameterBuilder.create(name, Type.DECIMAL)
+                        .withMinimum(BigDecimal.valueOf(0L)).withMaximum(BigDecimal.valueOf(99L)).withUnit("Cel")
+                        .withStepSize(BigDecimal.valueOf(0.25)).withGroupName("openWindowFunction")
+                        .withAdvanced(true).withDefault("5");
+                    break;
+                case CONFIG_PARAM_OPEN_WINDOW_DROP_TIME_RANGE:
+                    builder = ConfigDescriptionParameterBuilder.create(name, Type.INTEGER)
+                        .withMinimum(BigDecimal.valueOf(0L)).withUnit("s")
+                        .withStepSize(BigDecimal.valueOf(1)).withGroupName("openWindowFunction")
+                        .withAdvanced(true).withDefault("900");
+                    break;
+                case CONFIG_PARAM_OPEN_WINDOW_INC_TEMP_THR:
+                    builder = ConfigDescriptionParameterBuilder.create(name, Type.DECIMAL)
+                        .withMinimum(BigDecimal.valueOf(0L)).withMaximum(BigDecimal.valueOf(99L)).withUnit("Cel")
+                        .withStepSize(BigDecimal.valueOf(0.25)).withGroupName("openWindowFunction")
+                        .withAdvanced(true).withDefault("3");
+                    break;
+                case CONFIG_PARAM_OPEN_WINDOW_INC_TIME_RANGE:
+                    builder = ConfigDescriptionParameterBuilder.create(name, Type.INTEGER)
+                        .withMinimum(BigDecimal.valueOf(0L)).withUnit("s")
+                        .withStepSize(BigDecimal.valueOf(1)).withGroupName("openWindowFunction")
+                        .withAdvanced(true).withDefault("900");
+                    break;
+                case CONFIG_PARAM_OPEN_WINDOW_MAX_TIME:
+                    builder = ConfigDescriptionParameterBuilder.create(name, Type.INTEGER)
+                        .withMinimum(BigDecimal.valueOf(0L)).withUnit("s")
+                        .withStepSize(BigDecimal.valueOf(1)).withGroupName("openWindowFunction")
+                        .withAdvanced(true).withDefault("3600");
+                    break;
+                default:
+                    throw new IllegalStateException("Unimplemented config description parameter: " + name());
+
+            }
+            if (bundle == null || provider == null || locale == null) {
+                builder.withLabel(defaultLabel).withDescription(defaultDescr);
+            } else {
+                builder.withLabel(provider.getText(bundle, labelKey, defaultLabel, locale));
+                builder.withDescription(provider.getText(bundle, descrKey, defaultDescr, locale));
+            }
+            return builder.build();
+        }
+
+        @Override
+        public String toString() {
+            return name;
+        }
+
+        public static DescriptionParameter typeOf(String name) {
+            for (DescriptionParameter parameter : values()) {
+                if (parameter.name.equals(name)) {
+                    return parameter;
+                }
+            }
+            throw new IllegalArgumentException('"' + name + "\" is not a valid " + DescriptionParameter.class.getSimpleName());
+        }
     }
 }
